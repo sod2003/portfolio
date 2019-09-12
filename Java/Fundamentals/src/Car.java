0a90b1303ff83c508b275c090d18e182fe271e6e
Sean O'Dowd <sod2003@gmail.com>
2019-09-11 21:43:39 -0400

    Updating Car.java and Main.java

--- Java/Fundamentals/src/Car.java
+++ Java/Fundamentals/src/Car.java
@@ -2,9 +2,22 @@ import java.awt.*;
 
 public class Car {
 
-    int averageMilesPerGallon;
+    double averageMilesPerGallon;
     String licensePlate;
     Color paintColor;
     boolean areTailLightsWorking;
 
+    public Car(double averageMilesPerGallon,
+               String licensePlate,
+               Color paintColor,
+               boolean areTailLightsWorking) {
+        this.averageMilesPerGallon = averageMilesPerGallon;
+        this.licensePlate = licensePlate;
+        this.paintColor = paintColor;
+        this.areTailLightsWorking = areTailLightsWorking;
+    }
+
+    void changePaintColor(Color newPaint) {
+        this.paintColor = newPaint;
+    }
 }
--- Java/Fundamentals/src/Main.java
+++ Java/Fundamentals/src/Main.java
@@ -1,6 +1,21 @@
+import java.awt.*;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Car car = new Car(15.5, "CAY75", Color.BLACK, true);
+        Car myCar = new Car(20.3, "N00B", Color.BLUE, true);
+        System.out.println("My Car's license plate is: " + car.licensePlate);
+        System.out.println("My Car's color is: " + car.paintColor.toString());
+        System.out.println("Changing my Car's paint color...");
+        car.changePaintColor(Color.GREEN);
+        System.out.println("My Car's new paint color is: " + car.paintColor.toString());
+        System.out.println("Enter a word");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.next();
+        String uppercased = userInput.toUpperCase();
+        System.out.println(uppercased);
     }
 }
