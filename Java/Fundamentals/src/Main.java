c4c581e52936829dca676fbccc1de805f8762231
Sean O'Dowd <sod2003@gmail.com>
2019-09-17 23:53:03 -0400

    Updating the Java Fundamentals folder

--- Java/Fundamentals/src/Main.java
+++ Java/Fundamentals/src/Main.java
@@ -7,27 +7,29 @@ public class Main {
         /*
         System.out.println("Hello World!");
         Car car = new Car(15.5, "CAY75", Color.BLACK, true);
-
-        System.out.println("My Car's license plate is: " + car.licensePlate);
-        System.out.println("My Car's color is: " + car.paintColor.toString());
-        System.out.println("Changing my Car's paint color...");
-        car.changePaintColor(Color.GREEN);
-        System.out.println("My Car's new paint color is: " + car.paintColor.toString());
-        System.out.println("Enter a word");
-        Scanner sc = new Scanner(System.in);
-        String userInput = sc.next();
-        String uppercased = userInput.toUpperCase();
-        System.out.println(uppercased);
-        double myCarSpeed = 50;
-        myCarSpeed = myCar.speedingUp(myCarSpeed);
-        System.out.println("My car is speeding up to " + myCarSpeed);
          */
 
+        /*
         Car myCar = new Car(20.3, "N00B1", "Blue", false);
         System.out.println("My car is " + myCar.paintColor.toString() + " with " + myCar.averageMilesPerGallon + "MPG and do the tail lights work? " + myCar.areTailLightsWorking);
         myCar.tuneUp();
         myCar.changePaintColor("Black");
         myCar.fixTailights();
         System.out.println("Now after getting work done, my car is " + myCar.paintColor.toString() + " with " + myCar.averageMilesPerGallon + "MPG and do the tail lights work? " + myCar.areTailLightsWorking);
+        */
+
+        /*
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your age");
+        int age = sc.nextInt();
+        if(age <= 5) {
+            System.out.println("You're a baby!");
+        } else if(age > 5 && age < 18) {
+            System.out.println("You're a kid!");
+        } else { System.out.println("You're an adult!"); }
+        */
+
+        double power = Math.pow(5, 3);
+        System.out.println("5 raised to the power of 3 is: " + power);
     }
 }
