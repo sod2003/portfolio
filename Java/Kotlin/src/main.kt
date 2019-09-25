df185eee462698205a7a7d5b082fad26df0dc4a0
Sean O'Dowd <sod2003@gmail.com>
2019-09-24 23:55:56 -0400

    Updating main.kt

--- Java/Kotlin/src/main.kt
+++ Java/Kotlin/src/main.kt
@@ -1,3 +1,7 @@
 fun main() {
     println("Hello World!")
+    var myInt = 42
+    var myDouble = myInt.toDouble()
+    println("myInt is $myInt")
+    println("Upcasting to myDouble is $myDouble")
 }
