aa0c819f9204185910d9c2303d80b744436f6d9d
Sean O'Dowd <sod2003@gmail.com>
2019-09-26 23:56:10 -0400

    Updating main.kt

--- Java/Kotlin/src/main.kt
+++ Java/Kotlin/src/main.kt
@@ -7,4 +7,16 @@ fun main() {
     var myDouble = myInt.toDouble()
     println("myInt is $myInt")
     println("Upcasting to myDouble is $myDouble")
+
+    var itsFunctional = addList(1, 2, 3)
+    println("itsFunctional returns $itsFunctional")
+}
+
+// Now for a cool function that allows a variable number of parameters
+fun addList(vararg list: Int): Int {
+    var sum = 0
+    for ( i in list) {
+        sum += i
+    }
+    return sum
 }
