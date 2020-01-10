a63bb8146c0c4bfac187020e729b158b15c7e03f
sod2003 <sod2003@gmail.com>
2020-01-09 20:52:36 -0500

    Adding modules.py example

---
+++ python/fundamentals/modules.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import sys, os, random, datetime
+
+def main():
+    v = sys.version_info
+    print('Python version installed is {}.{}.{}'.format(*v))
+    y = sys.platform
+    print('The platform is {}'.format(y))
+
+    oper = os.name
+    print('The operating system is {}'.format(oper))
+    path = os.getenv('PATH')
+    print('The PATH environment variable contains: {}'.format(path))
+    curr_dir = os.getcwd()
+    print('The current working directory is: {}'.format(curr_dir))
+    hash = os.urandom(25).hex()
+    print(hash)
+
+    rando = random.randint(1, 1000)
+    print(rando)
+    x = list(range(25))
+    print(x)
+    random.shuffle(x)
+    print('Now, shuffled: {}'.format(x))
+
+    now = datetime.datetime.now()
+    print(now)
+    print(now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond)
+
+if __name__ == '__main__': main()
