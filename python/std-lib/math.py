a5244ad903d6f507b55f2d2c81cac835a6a23aa4
sod2003 <sod2003@gmail.com>
2020-04-05 19:07:42 -0400

    Adding math.py

---
+++ python/std-lib/math.py
@@ -0,0 +1,17 @@
+# Math module testing
+import math
+
+# Constants
+print("Pi:", math.pi)
+print("e:", math.e)
+print("NaN:", math.nan)
+print("Infinity:", math.inf)
+print("Negative Infinity:", -math.inf)
+
+# Trigonometry
+direction = math.cos(math.pi/4)
+print(direction)
+
+# Ceiling and Floor
+print("Ceiling is", math.ceil(10.3))
+print("Floor is", math.floor(15.5))
