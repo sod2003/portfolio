01b4828139cec3bbf5356b56dfdf37c71257fc05
sod2003 <sod2003@gmail.com>
2020-04-06 22:06:24 -0400

    Updating math.py

--- python/std-lib/math.py
+++ python/std-lib/math.py
@@ -15,3 +15,14 @@ print(direction)
 # Ceiling and Floor
 print("Ceiling is", math.ceil(10.3))
 print("Floor is", math.floor(15.5))
+
+# factorial and square root
+print("Factorial of 3 is", math.factorial(3))
+print("Square root of 64 is", math.sqrt(64))
+
+# GCD
+print("GCD between 52 and 8 is", math.gcd(52, 8))
+
+# Degrees and Radians
+print("Radians in a full circle:", math.radians(360))
+print("Degrees in a full circle:", math.degrees(math.pi * 2))
