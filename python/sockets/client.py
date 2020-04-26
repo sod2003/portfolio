d1cb307b20a60909ddbe8c3086c2ce8b9c1db4d7
sod2003 <sod2003@gmail.com>
2020-04-25 22:58:11 -0400

    Adding headers for buffering to server.py and client.py messages

--- python/sockets/client.py
+++ python/sockets/client.py
@@ -1,13 +1,27 @@
 import socket
 
+HEADERSIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create socket object that will use IPv4
 s.connect((socket.gethostname(), 1234)) # Same information as used in the server bind, but we want the client to connect to the server
 
-full_msg = ''
 while True:
-    msg = s.recv(8) # Receive the server connect msg with a 1024 buffer
-    if len(msg) <= 0:
-        break
-    full_msg += msg.decode('utf-8')
+
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16) # Receive the server connect msg with a 16 byte buffer
+        if new_msg:
+            print(f'new message length: {msg[:HEADERSIZE]}')
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False # We only need to get the message length on the initial pass through
+
+        full_msg += msg.decode('utf-8')
+
+        if len(full_msg) - HEADERSIZE == msglen:
+            print('full message received')
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ''
 
 print(full_msg)
--- python/sockets/server.py
+++ python/sockets/server.py
@@ -1,4 +1,6 @@
-import socket
+import socket, time
+
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create socket object that will use IPv4
 s.bind((socket.gethostname(), 1234)) # Binding to localhost and using a random port number that is unlikely to be in use
@@ -7,5 +9,12 @@ s.listen(5) # This provides a listening queue of 5 for the server
 while True:
     clientsocket, address = s.accept() # Will accept a connection from another socket object
     print(f'Connection from {address} has been established')
-    clientsocket.send(bytes('Welcome to the server!', 'utf-8'))
-    clientsocket.close()
+    msg = 'Welcome to the server!'
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg # The HEADER is the length of message
+    clientsocket.send(bytes(msg, 'utf-8'))
+    
+    while True:
+        time.sleep(3)
+        msg = f'The time is {time.time()}'
+        msg = f'{len(msg):<{HEADERSIZE}}' + msg
+        clientsocket.send(bytes(msg, 'utf-8'))
