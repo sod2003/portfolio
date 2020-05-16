54726979a71da4de073fd4ef82068c5867f86ad3
sod2003 <sod2003@gmail.com>
2020-05-16 17:43:00 -0400

    Updating chat_server.py

--- python/sockets/chat_server.py
+++ python/sockets/chat_server.py
@@ -7,6 +7,7 @@ PORT = 1234
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Setting the socket option of reuse to true
 
+# Binding server obj to IP and initiating operation
 server_socket.bind((IP, PORT))
 server_socket.listen()
 
