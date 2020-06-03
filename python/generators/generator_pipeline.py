f615adc817c8a5914dfb01127f4c8a7aa7eacbe0
sod2003 <sod2003@gmail.com>
2020-06-02 23:47:53 -0400

    Correcting argument passed in generator_pipeline.py

--- python/generators/generator_pipeline.py
+++ python/generators/generator_pipeline.py
@@ -2,7 +2,7 @@
 names = ['Sean', 'Jason', 'Sam', 'Jordan', 'Ali']
 
 def longest_name(name_list):
-    lengths = ((name, len(name)) for name in names)
+    lengths = ((name, len(name)) for name in name_list)
     longest = max(lengths, key=lambda x:x[1])          # The key argument is targeting the second set of values within the lengths variable 
     return longest
 
