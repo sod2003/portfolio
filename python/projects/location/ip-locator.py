4ad0d725a73f0ff5bc31c60965b5e9bbb67d3e29
sod2003 <sod2003@gmail.com>
2020-06-09 23:01:46 -0400

    Adding ip-locator.py example for geolocation project

---
+++ python/projects/location/ip-locator.py
@@ -0,0 +1,11 @@
+# Geolocation from IP lookups
+from urllib import request
+import json
+
+try:
+    data = json.load(request.urlopen('http://ipinfo.io/json'))
+except Exception as e:
+    print(e)
+finally:
+    print('You are near: {city}, {region}, {country}'.format(**data))
+    print(' Lat/Long: {}E'.format(data['loc'].replace(',','N, ')))
