a2551d94f6d1720c08f8c5250a06f958622589c6
Sean O'Dowd <sod2003@gmail.com>
2021-09-30 17:47:49 -0400

    Adding protocol examples

--- Swift/Exercise Files/AdventureSwift_Starter/Ch7_Leveling_Up.playground/Pages/Introducing protocols.xcplaygroundpage/Contents.swift
+++ Swift/Exercise Files/AdventureSwift_Starter/Ch7_Leveling_Up.playground/Pages/Introducing protocols.xcplaygroundpage/Contents.swift
@@ -15,7 +15,64 @@
 
  */
 // Declare a protocol
+protocol Collectable {
+    var name: String { get }
+    var price: Int { get set }
+    
+    init(withName: String, startingPrice: Int)
+    func collect() -> Bool
+}
 
+extension Collectable {
+    var priceIncrease: Int {
+        return self.price * 10
+    }
+    
+    init(name: String) {
+        self.init(withName: name, startingPrice: 100)
+    }
+    
+    func collect() -> Bool {
+        print("Default item couldn't be collected...")
+        return false
+    }
+}
 
+protocol Usable {
+    
+    func use()
+}
 // Protocol adoption
+class Item: Collectable, Usable {
+    var name: String
+    var price: Int
+    
+    required init(withName: String, startingPrice: Int) {
+        self.name = withName
+        self.price = startingPrice
+    }
+    
+    func collect() -> Bool {
+        print("Item collected!")
+        return true
+    }
+    
+    func use() {
+        print("Item used!")
+    }
+}
 
+let potion = Item(withName: "High Potion", startingPrice: 35)
+potion.collect()
+potion.use()
+
+let antidote = Item(name:"Antidote")
+print("\(antidote.price)")
+
+extension String {
+    func fancyDebug() {
+        print("This string has \(self.count) characters")
+    }
+}
+
+antidote.name.fancyDebug()
