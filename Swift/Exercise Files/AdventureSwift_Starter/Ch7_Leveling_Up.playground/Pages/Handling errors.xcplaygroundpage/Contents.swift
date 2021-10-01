28925528bf4aadd4e9b7389fed53e605bef5e4d0
Sean O'Dowd <sod2003@gmail.com>
2021-10-01 16:08:14 -0400

    Finishing Swift Exercises

--- Swift/Exercise Files/AdventureSwift_Starter/Ch7_Leveling_Up.playground/Pages/Handling errors.xcplaygroundpage/Contents.swift
+++ Swift/Exercise Files/AdventureSwift_Starter/Ch7_Leveling_Up.playground/Pages/Handling errors.xcplaygroundpage/Contents.swift
@@ -19,7 +19,7 @@ enum DataError: Error {
 }
 
 
-func loadData(path: String) throws {
+func loadData(path: String) throws -> Bool {
     guard path.contains("/") else {
         throw DataError.InvalidPath
     }
@@ -27,9 +27,36 @@ func loadData(path: String) throws {
     guard !path.isEmpty else {
         throw DataError.EmptyPath
     }
+    return true
 }
 
+let playerDataPath = "/example/data.txt"
+
 // Do-Catch statements
+do {
+    try loadData(path: playerDataPath)
+    print("This worked!")
+} catch is DataError {
+    print("Invalid or empty path detected...")
+} catch {
+    print("Unknown error detected...")
+}
 
-
+if let dataLoaded = try? loadData(path: playerDataPath) {
+    print("Data fetch was successful...")
+}
 // Propagating errors
+func propagateError() throws {
+    try loadData(path: playerDataPath)
+}
+
+do {
+    try propagateError()
+    print("Propagated data fetch successful...")
+} catch DataError.EmptyPath {
+    print("Empty path detected...")
+} catch DataError.InvalidPath {
+    print("Invalid path detected...")
+} catch {
+    print("Unknown error...")
+}
--- Swift/Exercise Files/Ch07/Ch07_Challenge.playground/Pages/Starter.xcplaygroundpage/Contents.swift
+++ Swift/Exercise Files/Ch07/Ch07_Challenge.playground/Pages/Starter.xcplaygroundpage/Contents.swift
@@ -15,15 +15,43 @@
  
  */
 // 1
+enum ActionError: Error {
+    case InsufficientMP
+    case OutOfRange
+    case UnknownError
+}
 
-// 2
-
-// 3
-
-// 4
-
-// 5
+// 2 // 3
+func attackEnemy(mp: Int, distance: Double) throws -> Bool? {
+    
+    // 4
+    guard mp >= 10 else {
+        throw ActionError.InsufficientMP
+    }
+    
+    guard distance < 5.9 else {
+        throw ActionError.OutOfRange
+    }
+    
+    // 5
+    return true
+}
 
 // 6
+do {
+    try attackEnemy(mp: 10, distance: 6.1)
+    print("Attack works!")
+} catch ActionError.InsufficientMP {
+    print("Not enough MP...")
+} catch ActionError.OutOfRange {
+    print("Enemy out of range...")
+} catch {
+    print("Unknown action error...")
+}
 
 // 7
+if let attackSuccess = try? attackEnemy(mp: 11, distance: 4.0) {
+    print("Attack successful? \(attackSuccess)")
+} else {
+    print("Attack wasn't successful.")
+}
