bbc27e728da7a84fdcaeef871daf204ae1a21941
Sean O'Dowd <sod2003@gmail.com>
2021-10-04 16:27:38 -0400

    Adding SwiftUI Project for XCode demo

---
+++ Swift/SwiftUIProject/SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by Sean O'Dowd on 10/4/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
