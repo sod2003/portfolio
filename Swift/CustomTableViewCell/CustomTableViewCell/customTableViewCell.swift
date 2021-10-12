75cf32b699786eb303953d12a003a32664fb3436
Sean O'Dowd <sod2003@gmail.com>
2021-10-12 12:07:05 -0400

    Adding CustomTableViewCell example

---
+++ Swift/CustomTableViewCell/CustomTableViewCell/customTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  customTableViewCell.swift
+//  CustomTableViewCell
+//
+//  Created by Sean O'Dowd on 10/12/21.
+//
+
+import UIKit
+
+class customTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
