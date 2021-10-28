fadb8619a301acd5d690862b14318bfe2c09bc35
Sean O'Dowd <sod2003@gmail.com>
2021-10-28 16:56:31 -0400

    Starting SwiftUI learning example project

---
+++ Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPizza
+//
+//  Created by Sean O'Dowd on 10/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/SwiftUIPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPizzaApp.swift
+//  SwiftUIPizza
+//
+//  Created by Sean O'Dowd on 10/28/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
