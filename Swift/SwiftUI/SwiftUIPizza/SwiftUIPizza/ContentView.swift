0fadbe2664501551bde6efee9eeeb4883501152b
Sean O'Dowd <sod2003@gmail.com>
2021-10-29 13:56:55 -0400

    Added img asset to SwiftUI folder and set base ContentView

--- Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/ContentView.swift
+++ Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("SwiftUI Pizza Company")
+            Image("monkey_base")
+            Text("Order pizza")
+            Spacer()
+        }
     }
 }
 
