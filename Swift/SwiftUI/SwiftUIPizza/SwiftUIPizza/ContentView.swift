1d0a32139830040c75b1ec47f8602ad5d6ca7983
Sean O'Dowd <sod2003@gmail.com>
2021-11-01 17:01:51 -0400

    Adding pizza slice img resources, and including a ForEach component within the pizza list to display the pizza slice count

--- Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/ContentView.swift
+++ Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/ContentView.swift
@@ -25,7 +25,14 @@ struct ContentView: View {
             List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
                 HStack(alignment: .top, spacing: 15){
                     Image("1_100w")
-                    Text("Chicken Pizza")
+                    VStack {
+                        Text("Chicken Pizza")
+                        HStack {
+                            ForEach(0..<4) {item in
+                                Image("Pizza Slice")
+                            }
+                        }
+                    }
                     Spacer()
                 }
             }
