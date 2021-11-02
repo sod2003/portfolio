f82fe24e82d16967013ec065fed39e95323a0d1e
Sean O'Dowd <sod2003@gmail.com>
2021-11-02 15:30:26 -0400

    Added layout priority modifiers to the ContentView

--- Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/ContentView.swift
+++ Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/ContentView.swift
@@ -11,11 +11,14 @@ struct ContentView: View {
     var body: some View {
         VStack {
             ContentHeaderView()
+                .layoutPriority(2)
             PageTitleView(title: "Order Pizza")
             MenuListView()
+                .layoutPriority(1)
             OrderListView()
+                .layoutPriority(1)
             
-            Spacer()
+            //Spacer()
         }
             .padding()
     }
