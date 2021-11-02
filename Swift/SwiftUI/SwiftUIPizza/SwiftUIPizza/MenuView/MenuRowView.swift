321090e505af58312ee1f0a39ab93f99cbde4068
Sean O'Dowd <sod2003@gmail.com>
2021-11-02 14:40:49 -0400

    Replacing pizza slice img in ratings with a system custom symbol

--- Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/MenuView/MenuRowView.swift
+++ Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/MenuView/MenuRowView.swift
@@ -11,7 +11,7 @@ struct MenuRowView: View {
     var body: some View {
         HStack(alignment: .top, spacing: 15){
             Image("1_100w")
-            VStack {
+            VStack(alignment: .leading) {
                 Text("Chicken Pizza")
                     .font(.title)
                     .fontWeight(.light)
