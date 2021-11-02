261d8ef764aa333480fdf4f7692b153c7de607c9
Sean O'Dowd <sod2003@gmail.com>
2021-11-02 15:09:01 -0400

    Exploring Image modifiers and Effects on MenuRowView img

--- Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/MenuView/MenuRowView.swift
+++ Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/MenuView/MenuRowView.swift
@@ -11,6 +11,10 @@ struct MenuRowView: View {
     var body: some View {
         HStack(alignment: .top, spacing: 15){
             Image("1_100w")
+//                .cornerRadius(10)
+//                .border(Color("G4"), width: 2)
+                .clipShape(Capsule())
+                .shadow(color: Color.black.opacity(0.5), radius: 5, x: 5, y: 5)
             VStack(alignment: .leading) {
                 Text("Chicken Pizza")
                     .font(.title)
