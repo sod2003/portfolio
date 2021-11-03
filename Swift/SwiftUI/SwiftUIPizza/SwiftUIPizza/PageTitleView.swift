13280be96298fa0aed62273d83eb6db8220db7b6
Sean O'Dowd <sod2003@gmail.com>
2021-11-03 15:39:45 -0400

    Added State variable isMenuDisplayed to ContentView, connected to new Button with toggle action, and added ternary operators to flip between Menu and Order lists in ContentView. Added isDisplayingOrders and modified overlay image based on values in PageTitleView.

--- Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/PageTitleView.swift
+++ Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/PageTitleView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct PageTitleView: View {
     var title:String
+    var isDisplayingOrders:Bool? = nil
     var body: some View {
         HStack {
             Spacer()
@@ -17,13 +18,14 @@ struct PageTitleView: View {
                 .fontWeight(.heavy)
                 .padding(.trailing)
         }.overlay(
-            Image(systemName: "chevron.up.square")
+            Image(systemName: isDisplayingOrders ?? false ? "chevron.up.square" : "chevron.down.square")
+                .foregroundColor(isDisplayingOrders != nil ? Color("G1") : .clear)
                 .font(.title)
                 .padding()
             , alignment: .leading
         )
-        .background(Color("G1"))
-        .foregroundColor(Color("G4"))
+        .background(Color("G4"))
+        .foregroundColor(Color("G1"))
     }
 }
 
