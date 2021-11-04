1b6d1b0201c2090626ae06cad8afc86805e7be9a
Sean O'Dowd <sod2003@gmail.com>
2021-11-04 13:29:45 -0400

    Added OrderModel to root views as a State variable of ContentView

--- Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/SwiftUIPizzaApp.swift
+++ Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/SwiftUIPizzaApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIPizzaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(orderModel: OrderModel())
         }
     }
 }
