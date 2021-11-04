6b64d2f709e85dbdbbaa2435eeca52e4aabd9fbf
Sean O'Dowd <sod2003@gmail.com>
2021-11-04 15:19:47 -0400

    Converting the binding to ObservedObject in OrderModel. Adding Order total to ListHeaderView.

--- Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/ContentView.swift
+++ Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var orderModel:OrderModel
+    @ObservedObject var orderModel:OrderModel
     @State var isMenuDisplayed:Bool = true
     var body: some View {
         VStack {
@@ -18,7 +18,7 @@ struct ContentView: View {
                 PageTitleView(title: "Order Pizza", isDisplayingOrders: isMenuDisplayed)
             }
             
-            MenuListView(orderModel: $orderModel)
+            MenuListView(orderModel: orderModel)
                 .layoutPriority(isMenuDisplayed ? 1.0 : 0.5)
             OrderListView(orderModel: orderModel)
                 .layoutPriority(isMenuDisplayed ? 0.5 : 1.0)
--- Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/MenuView/MenuListView.swift
+++ Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/MenuView/MenuListView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct MenuListView: View {
-    @Binding var orderModel:OrderModel
+    @ObservedObject var orderModel:OrderModel
     var menuList = MenuModel().menu
     var body: some View {
         VStack {
-            ListHeaderView(text: "Menu")
+            ListHeaderView(orderModel: orderModel, text: "Menu")
             NavigationView{
                 List(menuList) { item in
-                    NavigationLink(destination: MenuDetailView(orderModel: self.$orderModel, menuItem: item)){
+                    NavigationLink(destination: MenuDetailView(orderModel: self.orderModel, menuItem: item)){
                         MenuRowView(menuItem: item)
                             .listRowInsets(EdgeInsets())
                     }
@@ -28,6 +28,6 @@ struct MenuListView: View {
 
 struct MenuListView_Previews: PreviewProvider {
     static var previews: some View {
-        MenuListView(orderModel: .constant(OrderModel()))
+        MenuListView(orderModel: OrderModel())
     }
 }
