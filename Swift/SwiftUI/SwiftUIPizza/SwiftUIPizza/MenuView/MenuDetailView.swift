5a1bc2c29509eb872de4d079dd2799ec3d68efd8
PCI Adm <pciadm@pcis-mbp.delta.rl.delta.com>
2021-11-16 14:53:01 -0500

    Adding GeometryReader to MenuDetailView for changing layout in landscape mode versus portrait mode.

--- Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/MenuView/MenuDetailView.swift
+++ Swift/SwiftUI/SwiftUIPizza/SwiftUIPizza/MenuView/MenuDetailView.swift
@@ -1,103 +1,125 @@
 //
 //  MenuDetailView.swift
 //  Pizza
-//
-//  Created by Steven Lipton on 9/2/19.
+//  adaptive version for landscape.
+//  creates two functions of views to be able to switch them between Hstack and Vstack.
+//  Created by Steven Lipton on 12/26/20.
 //  Copyright © 2019 Steven Lipton. All rights reserved.
 //
 
 import SwiftUI
 ///A `View`for entering in an order. Takes basic information about the order from `menuItem`
+
+var size:Size = .medium
 struct MenuDetailView: View {
-    @EnvironmentObject var settings: UserPreferences
+    @EnvironmentObject var settings:UserPreferences
     @ObservedObject var orderModel:OrderModel
     @State var didOrder:Bool = false
     @State var quantity:Int = 1
     var menuItem:MenuItem
     var formattedPrice:String{
-        String(format:"%3.2f",menuItem.price * Double(quantity) * settings.size.rawValue)
+        String(format:"%3.2f",menuItem.price * Double(quantity) * size.rawValue)
     }
     func addItem(){
 //        orderModel.add(menuID: menuItem.id)
         didOrder = true
     }
     
-
+    func isCompactPortrait(geo:GeometryProxy)->Bool{
+        return geo.size.height <= 414
+    }
     
-    var body: some View {
-        VStack {
-            PageTitleView(title: menuItem.name)
-            SelectedImageView(image: "\(menuItem.id)_250w")
-                .padding(5)
-                .layoutPriority(3)
+    func titleView()->some View{
+        return
+        
+        GeometryReader{ geometry in
             
-            Text(menuItem.description)
-                .lineLimit(5)
-                .padding()
-                .layoutPriority(3)
-                
-            Spacer()
-            SizePickerView(size: $settings.size)
-            QuantityStepperView(quantity: $quantity)
             HStack{
-                Text("Order:  \(formattedPrice)")
-                    .font(.headline)
-                Spacer()
-                Text("Order total: " + orderModel.formattedTotal)
-                    .font(.headline)
-            }
-            .padding()
-            HStack{
-                Spacer()
-                Button(action: addItem) {
-                   Text("Add to order")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .padding()
-                    .background(Color("G4"))
-                        .foregroundColor(Color("IP"))
-                        .cornerRadius(5)
-                }
-//                .alert(isPresented: $didOrder){
-//                    Alert(title: Text("Pizza Ordered!"), message: Text("You ordered a " + self.menuItem.name))
-//                }
-                .sheet(isPresented: $didOrder){
-                    ConfirmView(menuID: self.menuItem.id, isPresented: self.$didOrder, orderModel: self.orderModel, quantity: self.$quantity, size: self.$settings.size)
-                }
+                SelectedImageView(image: "\(self.menuItem.id)_250w")
+                    .padding(5)
+                Text(self.menuItem.description)
+                    .frame(width: geometry.size.width * 2/5)
+                    .padding()
                 Spacer()
             }
-            .padding(.top)
+        }
+    }
+    
+    func menuOptionsView()-> some View{
+       return  VStack{
+            SizePickerView(size:$settings.size)
+            QuantityStepperView(quantity:$quantity)
+            PageTitleView(title: "Order:  \(formattedPrice)")
             Spacer()
         }
-        
     }
-}
-
+        
+    
+    var body: some View {
+        GeometryReader{ geo in
+            VStack {
+                HStack{
+                    PageTitleView(title: self.menuItem.name)
+                    Button(action: self.addItem) {
+                        Text("Add to order")
+                        
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding([.leading, .trailing])
+                            .background(Color("G3"))
+                            .foregroundColor(Color("IP"))
+                            .cornerRadius(5)
+                    }
+                    .sheet(isPresented: self.$didOrder){
+                        ConfirmView(menuID: self.menuItem.id, isPresented: self.$didOrder, orderModel:self.orderModel, quantity: self.$quantity, size:self.$settings.size)
+                    }
+                }
+                if isCompactPortrait(geo: geo){
+                    HStack{
+                        self.titleView()
+                        self.menuOptionsView()
+                    }
+                } else {
+                    VStack{
+                        self.titleView()
+                        self.menuOptionsView()
+                    }
+                }
+            }//Root VStack
+            .padding(.top, 5)
+        }
+        
+    }// body
+}// MenuDetailView
 struct MenuDetailView_Previews: PreviewProvider {
     static var previews: some View {
         MenuDetailView(orderModel: OrderModel(), menuItem: testMenuItem)
-            .environmentObject(UserPreferences())
     }
 }
 
 struct QuantityStepperView: View {
     @Binding var quantity:Int
     var body: some View {
-        Stepper(value:$quantity, in:1...10){
-            Text("Quantity: \(quantity)")
+        Stepper(value: $quantity, in: 1...10){
+            Text("Quantity \(quantity)")
         }
+            .padding()
     }
 }
 
+
 struct SizePickerView: View {
+    
     @Binding var size:Size
-    let sizes:[Size] = [.small, .medium, .large]
+    let sizes:[Size] = [.small,.medium,.large]
     var body: some View {
-        Picker(selection:$size, label: Text("Pizza Size")){
+        Picker(selection:$size, label:Text("Pizza Size")){
             ForEach(sizes, id:\.self){ size in
                 Text(size.formatted()).tag(size)
             }
         }
         .pickerStyle(SegmentedPickerStyle())
+        .font(.headline)
     }
 }
+
