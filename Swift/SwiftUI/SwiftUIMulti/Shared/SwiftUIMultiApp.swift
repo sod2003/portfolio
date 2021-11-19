c369723eaf70561d7538afc32a45b7e73c51a777
Sean O'Dowd <sod2003@gmail.com>
2021-11-19 17:46:15 -0500

    Initializing Multiplatform SwiftUI example.

---
+++ Swift/SwiftUI/SwiftUIMulti/Shared/SwiftUIMultiApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIMultiApp.swift
+//  Shared
+//
+//  Created by Sean O'Dowd on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMultiApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            ContentView(orderModel: OrderModel())
+//                .environmentObject(UserPreferences())
+        }
+    }
+}
