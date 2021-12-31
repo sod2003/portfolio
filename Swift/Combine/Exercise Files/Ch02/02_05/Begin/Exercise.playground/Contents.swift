4d0e24ff1072136d751c9607e3ae837a7faa10fd
Sean O'Dowd <sod2003@gmail.com>
2021-12-31 12:16:01 -0500

    Writing publisher/subscriber examples using Subjects.

--- Swift/Combine/Exercise Files/Ch02/02_05/Begin/Exercise.playground/Contents.swift
+++ Swift/Combine/Exercise Files/Ch02/02_05/Begin/Exercise.playground/Contents.swift
@@ -12,21 +12,28 @@ import Foundation
 import Combine
 
 //(1) Declare an Int PassthroughSubject
-
+let subject = PassthroughSubject<Int,Never>()
 //(2) Attach a subscriber to the subject
-
+let subscription = subject
+    .sink {
+        print($0)
+    }
 
 //(3) Publish the value `94`, via the subject, directly
-
+subject.send(94)
 
 //(4) Connect subject to a publisher, and publish the value `29`
+Just(29)
+    .subscribe(subject)
 
-
-//(5) Declare amnother subject, a CurrentValueSubject, with an initial `I am a...` value cached
-
+//(5) Declare another subject, a CurrentValueSubject, with an initial `I am a...` value cached
+let anotherSubject = CurrentValueSubject<String, Never>("I am a ...")
 //(6) Attach a subscriber to the subject
-
+let anotherSubscription = anotherSubject
+    .sink {
+        print($0)
+    }
 
 //(7) Publish the value `Subject`, via the subject, directly
-
+anotherSubject.send("Subject")
 
