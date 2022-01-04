0a8c9d4c6820790b56c9b5d3fc04fe9f1a67dc0d
Sean O'Dowd <sod2003@gmail.com>
2022-01-04 11:34:16 -0500

    Completing challenge to put together publisher, subscriber, and operator.

--- Swift/Combine/Exercise Files/Ch02/02_08/Begin/Challenge.playground/Contents.swift
+++ Swift/Combine/Exercise Files/Ch02/02_08/Begin/Challenge.playground/Contents.swift
@@ -12,9 +12,15 @@ let button = UIButton(type: .custom)
 
 //(1) Create a publisher, which publishes the following boolean array
 let array = [true, false, false, false, false, true, true]
+let publisher = array.publisher
 
 //(2) Create a subscriber, to assign to the textfield's isEnabled property, when publisher emits new data, from the publisher
-
+let subscriber =  publisher.assign(to: \.isEnabled, on: button)
+button.publisher(for: \.isEnabled).sink{
+    print("Printing the assigned value of button: \($0)")
+}
 //(3) Add an operator, to drop the first 2 elements from the publisher, before the subscriber assigns to the button
-
+let _ = publisher.dropFirst(2).sink{
+    print($0)
+}
 
