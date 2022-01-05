0c2f9affc67e2eaad1ffd61b76345084b6a72163
Sean O'Dowd <sod2003@gmail.com>
2022-01-05 17:02:16 -0500

    Creating a dataTaskPublisher example to publish and subscribe to REST API data.

--- Swift/Combine/Exercise Files/Ch03/03_01/Begin/Exercise.playground/Contents.swift
+++ Swift/Combine/Exercise Files/Ch03/03_01/Begin/Exercise.playground/Contents.swift
@@ -15,6 +15,15 @@ struct Post: Codable{
 let samplePost = Post(userId: 1, id: 2, title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit", body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto")
 
 //(1) Create a `dataTaskPublisher`
-
+let url = URL(string: "http://jsonplaceholder.typicode.com/posts")
+let publisher = URLSession.shared.dataTaskPublisher(for: url!)
+    .map{$0.data}
+    .decode(type: Array<Post>.self, decoder: JSONDecoder())
 
 //(2) Subscribe to the publisher
+let cancellableSink = publisher
+    .sink(receiveCompletion: { completion in
+        print(String(describing: completion))
+    }, receiveValue: { value in
+        print("Returned value: \(value)")
+    })
