f5303400e77ebca7b72fd5dbdd7063cb28427953
Sean O'Dowd <sod2003@gmail.com>
2022-01-07 16:37:30 -0500

    Adding Combine example of a call to an API endpoint while filtering returned data.

--- Swift/Combine/Exercise Files/Ch03/03_05/Begin/Challenge.playground/Contents.swift
+++ Swift/Combine/Exercise Files/Ch03/03_05/Begin/Challenge.playground/Contents.swift
@@ -17,15 +17,20 @@ let emptyPost = Post(userId: 0, id: 0, title: "Empty", body: "No Results")
 
 //(1) Create a `dataTaskPublisher`
 let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
-
-
-
-
-
-
+let publisher = URLSession.shared.dataTaskPublisher(for: url!)
+    .map{$0.data}
+    .decode(type: Array<Post>.self, decoder: JSONDecoder())
+    .map{$0.first}
+    .replaceNil(with: emptyPost)
+    .compactMap({$0.title})
 
 //(2) Subscribe to the publisher
-
+let cancellableSink = publisher
+    .sink(receiveCompletion: { completion in
+        print(String(describing: completion))
+    }, receiveValue: { value in
+        print("Returned value: \(value)")
+    })
 
 
 
