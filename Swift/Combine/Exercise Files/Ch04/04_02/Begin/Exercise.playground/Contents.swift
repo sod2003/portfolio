abbc6834b9c0f1986dd41d1fe828285129a6a34f
Sean O'Dowd <sod2003@gmail.com>
2022-01-10 16:10:01 -0500

    Building a custom publisher example with Combine.

--- Swift/Combine/Exercise Files/Ch04/04_02/Begin/Exercise.playground/Contents.swift
+++ Swift/Combine/Exercise Files/Ch04/04_02/Begin/Exercise.playground/Contents.swift
@@ -7,27 +7,36 @@ There are times when your built-in publishers, operators and subscribers won’t
 import Foundation
 import Combine
 
-func isPrime<T: BinaryInteger>(_ n: T) -> T? {
-    guard n != 2 else { return n }
-    guard n % 2 != 0 && n > 1 else { return nil }
-
-    var i = 3
-    while i * i <= n {
-        if (Int(n) % i) == 0 {
-            return nil
+extension Publisher {
+    func isPrimeInteger<T: BinaryInteger>() ->  Publishers.CompactMap<Self, T> where Output == T {
+        compactMap {
+            self.isPrime($0)
+            
         }
-        i += 2
     }
+    
+    func isPrime<T: BinaryInteger>(_ n: T) -> T? {
+        guard n != 2 else { return n }
+        guard n % 2 != 0 && n > 1 else { return nil }
 
-    return n
+        var i = 3
+        while i * i <= n {
+            if (Int(n) % i) == 0 {
+                return nil
+            }
+            i += 2
+        }
+
+        return n
+    }
 }
+
 
 let numbers:[Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-numbers.forEach{ value in
-    if let primeValue = isPrime(value){
-        print(primeValue)
-    }
+numbers.publisher.isPrimeInteger()
+    .sink {
+    print($0)
 }
 
 
