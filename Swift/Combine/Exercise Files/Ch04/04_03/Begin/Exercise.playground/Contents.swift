0e039b8d5ba8d37ba5efb726f037e77d6fe2df22
Sean O'Dowd <sod2003@gmail.com>
2022-01-11 15:16:25 -0500

    Throttling publisher data using backpressure example.

--- Swift/Combine/Exercise Files/Ch04/04_03/Begin/Exercise.playground/Contents.swift
+++ Swift/Combine/Exercise Files/Ch04/04_03/Begin/Exercise.playground/Contents.swift
@@ -8,9 +8,22 @@ import Foundation
 import Combine
 
 let cityPublisher = (["San Jose", "San Francisco", "Menlo Park", "Palo Alto", ]).publisher
-
-let citySubscription = cityPublisher
-    .sink { print($0) }
+final class CitySubscriber: Subscriber{
+    func receive(subscription: Subscription) {
+        subscription.request(.max(3))
+    }
+    func receive(_ input: String) -> Subscribers.Demand {
+        print("City: \(input)")
+        return .none
+    }
+    func receive(completion: Subscribers.Completion<Never>) {
+        print("Subscription \(completion)")
+    }
+    typealias Input = String
+    typealias Failure = Never
+}
+let citySubscription = CitySubscriber()
+cityPublisher.subscribe(citySubscription)
 
 
 
