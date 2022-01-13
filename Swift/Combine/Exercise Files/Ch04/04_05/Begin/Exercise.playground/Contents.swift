7e0c7712aee0f6371c846f55f17bf5393c516dc7
Sean O'Dowd <sod2003@gmail.com>
2022-01-13 16:04:00 -0500

    Showcasing Combine's advanced operators.

--- Swift/Combine/Exercise Files/Ch04/04_05/Begin/Exercise.playground/Contents.swift
+++ Swift/Combine/Exercise Files/Ch04/04_05/Begin/Exercise.playground/Contents.swift
@@ -18,21 +18,21 @@ let words = (21...40).compactMap { String($0) }
     .publisher
 
 let cancellableSink = numbers
-    //.filter {$0 % 2 == 0}
-    //.compactMap{value in Float(value)}
-    //.first()
-    //.last(where: {$0 < 20 })
-    //.dropFirst()
-    //.drop(while: {$0 % 3 == 0} )
-    //.prefix(4)
-    //.prefix(while: {$0 < 5})
-    //.append(21,22,23)
-    //.prepend(21,22,23)
-    //.merge(with: numbersTwo)
-    //.combineLatest(words)
-    //.zip(numbersTwo)
-    //.collect()
-    //.throttle(for: .seconds(2), scheduler: DispatchQueue.main, latest: true)
+//    .filter {$0 % 2 == 0}
+//    .compactMap{value in Float(value)}
+//    .first()
+//    .last(where: {$0 < 20 })
+//    .dropFirst()
+//    .drop(while: {$0 < 5})
+//    .prefix(4)
+//    .prefix(while: {$0 < 5})
+//    .append(21,22,23)
+//    .prepend(21,22,23)
+//    .merge(with: numbersTwo)
+//    .combineLatest(words)
+//    .zip(numbersTwo)
+//    .collect()
+//    .throttle(for: .seconds(2), scheduler: DispatchQueue.main, latest: true)
     .sink {print($0)}
 
 
