74b417a742b6e9f3b80b89aa40369499ed8db31a
Sean O'Dowd <sod2003@gmail.com>
2022-01-25 16:33:04 -0500

    Adapting protocol to Struct in example.

--- Swift/Protocol_Oriented_Programming/Exercise Files/01_03/begin/Protocols.playground/Contents.swift
+++ Swift/Protocol_Oriented_Programming/Exercise Files/01_03/begin/Protocols.playground/Contents.swift
@@ -7,5 +7,30 @@ protocol Taggable {
     static var counter: Int { get }
     mutating func updateData( data: Data ) -> Bool
     static func incrementCounter()
+    
     init(tag: String, data: Data)
+}
+
+struct TaggedData: Taggable {
+    var tag: String
+    
+    var data: Data
+    
+    static var counter: Int = 0
+    
+    mutating func updateData(data: Data) -> Bool {
+        self.data = data
+        return true
+    }
+    
+    static func incrementCounter() {
+        counter += 1
+    }
+    
+    init(tag: String, data: Data) {
+        self.tag = tag
+        self.data = data
+    }
+    
+    
 }
