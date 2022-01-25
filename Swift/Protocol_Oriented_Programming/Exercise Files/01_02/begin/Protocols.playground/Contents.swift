507a72bdeddf042722aba61f3deb138866291dc9
Sean O'Dowd <sod2003@gmail.com>
2022-01-25 10:42:46 -0500

    Adding example of a Protocol in Swift

--- Swift/Protocol_Oriented_Programming/Exercise Files/01_02/begin/Protocols.playground/Contents.swift
+++ Swift/Protocol_Oriented_Programming/Exercise Files/01_02/begin/Protocols.playground/Contents.swift
@@ -1,1 +1,12 @@
+import Foundation
 
+protocol Taggable {
+    var tag: String { get set }
+    var data: Data { get }
+    static var counter: Int { get }
+    
+    mutating func update(data: Data) -> Bool
+    static func incrementCounter()
+    
+    init(tag: String, data: Data)
+}
