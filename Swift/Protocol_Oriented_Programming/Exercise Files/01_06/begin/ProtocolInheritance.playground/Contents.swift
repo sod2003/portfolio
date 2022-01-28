6afbbcd81a5e79eba87ed4a4d927fd71423b8041
Sean O'Dowd <sod2003@gmail.com>
2022-01-28 16:49:34 -0500

    Added example of Protocol inheritance.

--- Swift/Protocol_Oriented_Programming/Exercise Files/01_06/begin/ProtocolInheritance.playground/Contents.swift
+++ Swift/Protocol_Oriented_Programming/Exercise Files/01_06/begin/ProtocolInheritance.playground/Contents.swift
@@ -5,3 +5,12 @@ protocol Taggable {
     var data: Data { get }
     init(tag: String, data: Data)
 }
+
+protocol TaggedPersistable: Taggable {
+    init(tag: String, contentsOf url: URL) throws
+    func persist(to url: URL) throws
+}
+
+protocol TaggedEncodable: Taggable {
+    var base64: String { get }
+}
