94e319893dfb4a5e25152f2f4775baa3e0c865b2
Sean O'Dowd <sod2003@gmail.com>
2022-01-31 16:00:12 -0500

    Showing example of multiple protocol integration.

--- Swift/Protocol_Oriented_Programming/Exercise Files/01_07/begin/ProtocolComposition.playground/Contents.swift
+++ Swift/Protocol_Oriented_Programming/Exercise Files/01_07/begin/ProtocolComposition.playground/Contents.swift
@@ -1,16 +1,51 @@
 import Foundation
 
 protocol Taggable {
-    var tag: String {get}
+    var tag: String { get }
     var data: Data { get }
     init(tag: String, data: Data)
 }
 
-protocol TaggedPersistable: Taggable /*, CustomStringConvertible, Equatable*/{
+protocol TaggedPersistable: Taggable , CustomStringConvertible, Equatable{
     init(tag: String, contentsOf url: URL) throws
     func persist(to url: URL) throws
 }
 
 protocol TaggedEncodable: Taggable {
     var base64: String { get }
+}
+
+struct MyData: Taggable {
+    
+    var tag: String
+    
+    var data: Data
+    
+    init(tag: String, data: Data) {
+        self.tag = tag
+        self.data = data
+    }
+}
+
+extension MyData: CustomStringConvertible {
+    var description: String {
+        return "MyData(\(tag))"
+    }
+}
+
+extension MyData: TaggedEncodable {
+    var base64: String {
+        return self.data.base64EncodedString()
+    }
+}
+
+extension MyData: TaggedPersistable {
+    init(tag: String, contentsOf url: URL) throws {
+        let data = try Data.init(contentsOf: url)
+        self.init(tag: tag, data: data)
+    }
+    
+    func persist(to url: URL) throws {
+        try self.data.write(to: url)
+    }
 }
