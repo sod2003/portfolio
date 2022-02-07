8a5ae9e97ead791359b059285036cc335e676893
Sean O'Dowd <sod2003@gmail.com>
2022-02-07 18:05:52 -0500

    Adding an example of how code is repeated when not using generics

--- Swift/Protocol_Oriented_Programming/Exercise Files/03_01/begin/Generics.playground/Pages/Without Generics.xcplaygroundpage/Contents.swift
+++ Swift/Protocol_Oriented_Programming/Exercise Files/03_01/begin/Generics.playground/Pages/Without Generics.xcplaygroundpage/Contents.swift
@@ -2,8 +2,25 @@ import Foundation
 
 
 func equals(lhs: Int, rhs: Int) -> Bool {
-    // The iplementation is straightforward
+    // The implementation is straightforward
     return lhs == rhs
 }
 
 print(equals(lhs: 3, rhs: 4))
+
+//equals(lhs: 3, rhs: 4)
+
+func equals(lhs: Double, rhs: Double) -> Bool {
+    return lhs == rhs
+}
+
+equals(lhs: 1.4, rhs: 4.5)
+
+let pi: Float = 3.14
+let e: Float = 2.71
+
+func equals(lhs: Float, rhs: Float) -> Bool {
+    return lhs == rhs
+}
+
+equals(lhs: pi, rhs: e)
