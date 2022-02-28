2acd3ea619a81331614ca15deea6fcafeae61409
Sean O'Dowd <sod2003@gmail.com>
2022-02-28 18:53:01 -0500

    Presenting weather data in app by adding ObservedObject for weather data controller in ContentView and allowing user input

--- Swift/Protocol_Oriented_Programming/Exercise Files/04_08/begin/WeatherApp/WeatherApp/ContentView.swift
+++ Swift/Protocol_Oriented_Programming/Exercise Files/04_08/begin/WeatherApp/WeatherApp/ContentView.swift
@@ -11,14 +11,21 @@ import SwiftUI
 struct ContentView: View {
     @State private var input: String = ""
     
+    @ObservedObject var weatherViewModel = WeatherViewModel()
+    
     var body: some View {
         VStack {
-            TextField("Enter city", text: $input)
+            TextField("Enter City", text: $input, onEditingChanged: { (_) in
+            }, onCommit: {
+                if !self.input.isEmpty{
+                self.weatherViewModel.fetch(city: self.input)
+                }
+            })
             .font(.title)
             
             Divider()
             
-            Text(input)
+            Text(weatherViewModel.weatherInfo)
                 .font(.body)
         }.padding()
     }
