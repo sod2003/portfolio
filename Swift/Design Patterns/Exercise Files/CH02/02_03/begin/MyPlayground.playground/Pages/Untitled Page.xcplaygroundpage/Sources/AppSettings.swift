4c8d1ca34a08d8121a9ed2a2916612f2aab81d4e
Sean O'Dowd <sod2003@gmail.com>
2022-03-09 16:54:41 -0500

    Adding a readers-writer lock to my singleton example

--- Swift/Design Patterns/Exercise Files/CH02/02_03/begin/MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Sources/AppSettings.swift
+++ Swift/Design Patterns/Exercise Files/CH02/02_03/begin/MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Sources/AppSettings.swift
@@ -4,7 +4,7 @@ final public class AppSettings {
     
     public static let shared = AppSettings()
     
-    private let serialQueue = DispatchQueue(label: "serialQueue")
+    private let concurrentQueue = DispatchQueue(label: "concurrentQueue", attributes: .concurrent)
     
     private var settings: [String: Any] = ["Theme": "Dark", "MaxConcurrentDownloads": 4]
     
@@ -14,7 +14,7 @@ final public class AppSettings {
     
     public func string(forKey key: String) -> String? {
         var result: String?
-        serialQueue.sync {
+        concurrentQueue.sync {
             result = settings[key] as? String
         }
         return result
@@ -22,15 +22,15 @@ final public class AppSettings {
     
     public func int(forKey key: String) -> Int? {
         var result: Int?
-        serialQueue.sync {
+        concurrentQueue.sync {
             result = settings[key] as? Int
         }
         return result
     }
     
     public func set(value: Any, forKey key: String) {
-        serialQueue.sync {
-            settings[key] = value
+        concurrentQueue.async(flags: .barrier) {
+            self.settings[key] = value
         }
     }
 }
