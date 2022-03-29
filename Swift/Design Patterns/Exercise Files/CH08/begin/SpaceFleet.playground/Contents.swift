115df554f3b72448b0f32657680d631293b4b2ba
Sean O'Dowd <sod2003@gmail.com>
2022-03-28 20:25:00 -0400

    Creating a Flyweight example

--- Swift/Design Patterns/Exercise Files/CH08/begin/SpaceFleet.playground/Contents.swift
+++ Swift/Design Patterns/Exercise Files/CH08/begin/SpaceFleet.playground/Contents.swift
@@ -1,14 +1,24 @@
 import UIKit
 
-class SpaceShip {
+class SharedSpaceShipData {
+    
     private let mesh: [Float]
     private let texture: UIImage?
-    private var position: (Float, Float, Float)
-
-    init(mesh: [Float], imageNamed name: String, position: (Float, Float, Float) = (0, 0, 0)) {
+    
+    init(mesh: [Float], imageNamed name: String) {
         self.mesh = mesh
         self.texture = UIImage(named: name)
+    }
+}
+
+class SpaceShip {
+    
+    private var position: (Float, Float, Float)
+    private var intrinsicState: SharedSpaceShipData
+
+    init(sharedData: SharedSpaceShipData, position: (Float, Float, Float) = (0, 0, 0)) {
         self.position = position
+        self.intrinsicState = sharedData
     }
 }
 
@@ -17,9 +27,10 @@ let fleetSize = 1000
 var ships = [SpaceShip]()
 var vertices = [Float](repeating: 0, count: 1000) // just a dummy array of floats
 
+let sharedState = SharedSpaceShipData(mesh: vertices, imageNamed: "SpaceShip")
+
 for _ in 0..<fleetSize {
-    let ship = SpaceShip(mesh: vertices,
-                         imageNamed: "SpaceShip",
+    let ship = SpaceShip(sharedData: sharedState,
                          position: (Float.random(in: 1...100),
                                     Float.random(in: 1...100),
                                     Float.random(in: 1...100)))
