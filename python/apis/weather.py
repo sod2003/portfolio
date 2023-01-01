5aec119b4b0481125db79222736bf9fe727cfaa1
sod2003 <sod2003@gmail.com>
2023-01-01 14:04:25 -0500

    Converted weather.py program to take user input for the city

--- python/apis/weather.py
+++ python/apis/weather.py
@@ -1,16 +1,23 @@
 import requests, secret, datetime
 
-city = 'Atlanta'
 key = secret.key
 
 # Targeting the Open Weather Map api for my current weather request for the City of Atlanta
 # The API key is extracted to a hidden file that will not be included in this public repo; an API key can be obtained by signing up for an account at https://openweathermap.org
-request = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}')
+def fetch_weather(city):
+    request = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}')
 
-weather = request.json()
+    weather = request.json()
 
-dt = weather['dt']
-dt = datetime.datetime.utcfromtimestamp(dt//1000).replace(microsecond=dt%1000*1000)
-humidity = weather['main']['humidity']
+    dt = weather['dt']
+    dt = datetime.datetime.utcfromtimestamp(dt//1000).replace(microsecond=dt%1000*1000)
+    humidity = weather['main']['humidity']
 
-print(f'{dt} -> {city}\'s current Humidity: {humidity}')
+    print(f'{dt} -> {city}\'s current Humidity: {humidity}')
+
+def main():
+    city = input('Enter city name: ')
+    fetch_weather(city)
+
+if __name__ == '__main__':
+    main()
