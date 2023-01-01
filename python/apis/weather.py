70686e800a92528b2b2349990ecd034117c88ff8
sod2003 <sod2003@gmail.com>
2023-01-01 12:29:49 -0500

    Swapped out hardcoded weather for a request api call to Open Weather Map

--- python/apis/weather.py
+++ python/apis/weather.py
@@ -1,3 +1,13 @@
+import requests, secret
+
+city = 'atlanta'
+key = secret.key
+
+# Targeting the Open Weather Map api for my current weather request for the City of Atlanta
+# The API key is extracted to a hidden file that will not be included in this public repo; an API key can be obtained by signing up for an account at https://openweathermap.org
+request = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}')
+print(request)
+'''
 weather = [
     {
         'date':'today',
@@ -16,3 +26,4 @@ for forecast in weather:
     state = forecast['state']
     temp = forecast['temp']
     print(f"The weather for {date} will be {state} with a temperature of {temp} degrees.")
+'''
