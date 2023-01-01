7d89c6e3d1d171da7d0f79a6fa41724ed5e2183e
sod2003 <sod2003@gmail.com>
2023-01-01 13:43:53 -0500

    Formatting weather.py to print the formatted datetime and humidity from the current weather api call

--- python/apis/weather.py
+++ python/apis/weather.py
@@ -1,29 +1,16 @@
-import requests, secret
+import requests, secret, datetime
 
-city = 'atlanta'
+city = 'Atlanta'
 key = secret.key
 
 # Targeting the Open Weather Map api for my current weather request for the City of Atlanta
 # The API key is extracted to a hidden file that will not be included in this public repo; an API key can be obtained by signing up for an account at https://openweathermap.org
 request = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}')
-print(request)
-'''
-weather = [
-    {
-        'date':'today',
-        'state': 'cloudy',
-        'temp': 68.5
-    },
-    {
-        'date':'tomorrow',
-        'state': 'sunny',
-        'temp': 74.8
-    }
-]
 
-for forecast in weather:
-    date = forecast['date']
-    state = forecast['state']
-    temp = forecast['temp']
-    print(f"The weather for {date} will be {state} with a temperature of {temp} degrees.")
-'''
+weather = request.json()
+
+dt = weather['dt']
+dt = datetime.datetime.utcfromtimestamp(dt//1000).replace(microsecond=dt%1000*1000)
+humidity = weather['main']['humidity']
+
+print(f'{dt} -> {city}\'s current Humidity: {humidity}')
