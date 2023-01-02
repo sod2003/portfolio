86fb5ab460551f336f13edfec87435b905f74c13
sod2003 <sod2003@gmail.com>
2023-01-02 14:04:01 -0500

    Put list functionality into a function, added main function call, and created a function call for extracting place names from file names

--- python/projects/photo-edit/organize_photos.py
+++ python/projects/photo-edit/organize_photos.py
@@ -1,5 +1,19 @@
 import os
 
-os.chdir('Photos')
-originals = os.listdir()
-print(originals)
+def list_filenames():
+    os.chdir('Photos')
+    return os.listdir()
+
+def extract_filename(file):
+    first = file.find('_')
+    partial = file[first+1:]
+    second = partial.find('_')
+    return partial[:second]
+
+def main():
+    list = list_filenames()
+    for filename in list:
+        print(extract_filename(filename))
+
+if __name__ == "__main__":
+    main()
