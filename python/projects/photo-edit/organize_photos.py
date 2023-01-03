8ac86425c4920fd9b83b20049b468e964de39c71
sod2003 <sod2003@gmail.com>
2023-01-02 21:22:43 -0500

    Added new folder creation function

--- python/projects/photo-edit/organize_photos.py
+++ python/projects/photo-edit/organize_photos.py
@@ -7,10 +7,20 @@ def list_filenames():
 def extract_filename(file):
     return file.split('_')[1]
 
+def make_place_directories(places):
+    for place in places:
+        os.mkdir(place)
+
 def main():
     list = list_filenames()
+    places = []
     for filename in list:
-        print(extract_filename(filename))
+        place = extract_filename(filename)
+        if place not in places:
+            places.append(place)
+    print(places)
+    make_place_directories(places)
+    print(os.listdir())
 
 if __name__ == "__main__":
     main()
