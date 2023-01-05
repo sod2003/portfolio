18c88977d777099768ae19cbfcf40e8e600a7d0b
sod2003 <sod2003@gmail.com>
2023-01-04 21:25:37 -0500

    Editing for organization

--- python/projects/photo-edit/organize_photos.py
+++ python/projects/photo-edit/organize_photos.py
@@ -1,15 +1,19 @@
 import os
 
+
 def list_filenames(directory):
     os.chdir(directory)
     return os.listdir()
 
+
 def extract_filename(file):
     return file.split('_')[1]
+
 
 def make_place_directories(places):
     for place in places:
         os.mkdir(place)
+
 
 def organize_photos(directory):
     list = list_filenames(directory)
@@ -22,6 +26,7 @@ def organize_photos(directory):
     for file in list:
         place = extract_filename(file)
         os.rename(file, os.path.join(place, file))
+
 
 if __name__ == "__main__":
     organize_photos('Photos')
