068e95047871861c037b8348ab1e4f6fa1728cae
sod2003 <sod2003@gmail.com>
2023-01-06 22:47:41 -0500

    Added new profanity text file for testing more complicated scenarios. Edited the rudeword.py program to write to a new file when it filters profanity, bleeping out the rude words.

---
+++ python/projects/profanity-filter/my_other_story.txt
@@ -0,0 +1,1 @@
+Well, darn! This wasn't supposed to happen. Now I feel like a jerk. Darn it all to Heck!
--- python/projects/profanity-filter/rudewords.py
+++ python/projects/profanity-filter/rudewords.py
@@ -4,23 +4,38 @@ rude_words = ["crap", "darn", "heck", "jerk", "idiot", "butt", "devil"]
 
 def check_line(line):
     rude_count = 0
-    words = line.split(" ")    
+    words = line.split(" ")  
+    word_index = 0  
     for word in words:
-        word = word.strip(string.punctuation).lower()
-        if word in rude_words:
+        stripped_word = word.strip(string.punctuation).lower()
+        if stripped_word in rude_words:
             rude_count += 1
-            print(f"Found rude word: {word}")
-    return rude_count
+            print(f"Found rude word: {stripped_word}")
+            words[word_index] = bleeper(word)
+        word_index += 1
+    line = " ".join(words)
+    return line, rude_count
 
 def check_file(filename):
     with open(filename) as myfile:
         rude_count = 0
+        lines = []
         for line in myfile:
-            rude_count += check_line(line)
+            line, rude_subtotal = check_line(line)
+            rude_count += rude_subtotal
+            lines.append(line)
 
     if rude_count == 0:
         print("Congratulations, your file has no rude words.")
         print("At least, no rude words I know.")
+    else:
+        with open('bleeped_words.txt', 'w') as bleeped_words:
+            bleeped_words.write("\n".join(lines))
+        print(f"Found {rude_count} rude words in your file. See bleeped_words.txt for a censored copy of your file.")
+
+def bleeper(word):
+    stripped_word = word.strip(string.punctuation)
+    return word.replace(stripped_word, '*' * len(stripped_word))
 
 if __name__ == "__main__":
-    check_file("my_story.txt")
+    check_file("my_other_story.txt")
