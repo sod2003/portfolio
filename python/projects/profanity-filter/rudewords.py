2cc6e46c57a559a63435caba8ce8bf62a19c8f0b
sod2003 <sod2003@gmail.com>
2023-01-06 21:54:38 -0500

    Moved code into check_line and check_file functions. Converted program to search by word instead of by character.

--- python/projects/profanity-filter/rudewords.py
+++ python/projects/profanity-filter/rudewords.py
@@ -1,12 +1,23 @@
 rude_words = ["crap", "darn", "heck", "jerk", "idiot", "butt", "devil"]
 
-with open("my_story.txt") as myfile:
-    contents = myfile.read()
+def check_line(line):
     rude_count = 0
-    for rude in rude_words:
-        if rude in contents:
+    words = line.split(" ")    
+    for word in words:
+        if word in rude_words:
             rude_count += 1
-            print(f"Found rude word: {rude}")
-if rude_count == 0:
-    print("Congratulations, your file has no rude words.")
-    print("At least, no rude words I know.")
+            print(f"Found rude word: {word}")
+    return rude_count
+
+def check_file(filename):
+    with open(filename) as myfile:
+        rude_count = 0
+        for line in myfile:
+            rude_count += check_line(line)
+
+    if rude_count == 0:
+        print("Congratulations, your file has no rude words.")
+        print("At least, no rude words I know.")
+
+if __name__ == "__main__":
+    check_file("my_story.txt")
