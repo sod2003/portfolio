c02cb2aafb34e51d8fc4aac178d6f46a9908a9cd
sod2003 <sod2003@gmail.com>
2023-01-11 19:37:25 -0500

    Creating a Java project, and testing JavaDocs

---
+++ Java/Projects/src/myFirstProject/FirstClass.java
@@ -0,0 +1,8 @@
+package myFirstProject;
+
+public class FirstClass {
+
+	public static void main(String[] args){
+	    System.out.println("My first Java Application");
+	  }
+}
---
+++ Java/Projects/src/myFirstProject/javaDocExample.java
@@ -0,0 +1,15 @@
+package myFirstProject;
+
+/** This is my first JavaDoc
+ * 
+ * @author Sean O'Dowd
+ */
+public class javaDocExample {
+
+	/** This is a JavaDoc for a method
+	 * 
+	 */
+	public static void docMethod() {
+		
+	}
+}
