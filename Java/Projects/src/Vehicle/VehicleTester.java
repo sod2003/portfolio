0ee9da4400646eb1c74cdbcdee7ccde4e3817642
sod2003 <sod2003@gmail.com>
2023-01-12 21:48:56 -0500

    Adding Vehicle Package with classes of: Vehicle, VehicleTester, Car, Plane, and Boat.

---
+++ Java/Projects/src/Vehicle/VehicleTester.java
@@ -0,0 +1,16 @@
+package Vehicle;
+
+public class VehicleTester {
+
+	public static void main(String[] args) {
+		Vehicle[] vehicles = new Vehicle[3];
+		vehicles[0] = new Car();
+		vehicles[1] = new Plane();
+		vehicles[2] = new Boat();
+		for (int i = 0; i < vehicles.length; i++) {
+			Vehicle vehicle = vehicles[i];
+			vehicle.speed();
+		}
+	}
+
+}
