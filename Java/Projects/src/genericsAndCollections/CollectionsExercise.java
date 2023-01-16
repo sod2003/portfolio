b74892ef2be37d28be3583fdbd684fad7625c2cc
sod2003 <sod2003@gmail.com>
2023-01-16 11:07:44 -0500

    Adding IteratorExmple and CollectionsExercise classes to genericsAndCollections package in my Java project

---
+++ Java/Projects/src/genericsAndCollections/CollectionsExercise.java
@@ -0,0 +1,18 @@
+package genericsAndCollections;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CollectionsExercise {
+
+	public static void main(String[] args) {
+		List<String> listOfItems = new LinkedList<String>();
+		listOfItems.add("Mike");
+		listOfItems.add("Bob");
+		listOfItems.add("Alice");
+		
+		 for (String name : listOfItems) {
+			 System.out.println(name);
+	     }
+	}
+}
