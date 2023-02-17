4bb1d4004bd6270943c8440a687f7dfd67cabc12
sod2003 <sod2003@gmail.com>
2023-02-16 23:11:59 -0500

    Updated reflection/TestRunner to use a Custom ClassLoader for Test annotations

--- Java/Projects/src/reflection/TestRunner.java
+++ Java/Projects/src/reflection/TestRunner.java
@@ -1,40 +1,49 @@
 package reflection;
 
 import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class TestRunner {
 
-  private static final List<Class<?>> TESTS = List.of(CalculatorTest.class);
-
   public static void main(String[] args) throws Exception {
-
+    if (args.length != 2) {
+      System.out.println("Usage: TestRunner [test folder] [test name]");
+      return;
+    }
     List<String> passed = new ArrayList<>();
     List<String> failed = new ArrayList<>();
-
-    for (Class<?> klass : TESTS) {
-      if (!UnitTest.class.isAssignableFrom(klass)) {
-    	  throw new IllegalArgumentException("Class" + klass.toString() + " must implement UnitTest");
+    Class<?> testClass = getTestClass(args[0], args[1]);
+    for (Method method : testClass.getDeclaredMethods()) {
+      if (method.getAnnotation(Test.class) == null) {
+        continue;
       }
-
-      for (Method method : klass.getDeclaredMethods()) {
-    	  if (method.getAnnotation(Test.class) != null) {
-    		  try {
-    			  UnitTest test = (UnitTest) klass.getConstructor().newInstance();
-    			  test.beforeEachTest();
-    			  method.invoke(test);
-    			  test.afterEachTest();
-    			  passed.add(getTestName(klass, method));
-    		  } catch (Throwable throwable) {
-    			  failed.add(getTestName(klass, method));
-    		  }
-    	  }
+      try {
+        UnitTest test = (UnitTest) testClass.getConstructor().newInstance();
+        test.beforeEachTest();
+        method.invoke(test);
+        test.afterEachTest();
+        passed.add(getTestName(testClass, method));
+      } catch (Throwable throwable) {
+        failed.add(getTestName(testClass, method));
       }
     }
 
     System.out.println("Passed tests: " + passed);
     System.out.println("FAILED tests: " + failed);
+  }
+
+  private static Class<?> getTestClass(String testFolder, String className) throws Exception {
+    URL testDir = Path.of(testFolder).toUri().toURL();
+    URLClassLoader loader = new URLClassLoader(new URL[]{testDir});
+    Class<?> klass = Class.forName(className, true, loader);
+    if (!UnitTest.class.isAssignableFrom(klass)) {
+      throw new IllegalArgumentException("Class " + klass.toString() + " must implement UnitTest");
+    }
+    return klass;
   }
 
   private static String getTestName(Class<?> klass, Method method) {
