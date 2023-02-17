9744d819a4134c1df92a0559b4d092e09c2df06b
sod2003 <sod2003@gmail.com>
2023-02-16 23:11:12 -0500

    Added reflection2 pakage for Dynamic PRoxy example

---
+++ Java/Projects/src/reflection2/UdacisearchClient.java
@@ -0,0 +1,16 @@
+package reflection2;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+
+public interface UdacisearchClient {
+  String getName();
+  long getId();
+  int getQuarterlyBudget();
+  int getNumEmployees();
+  Instant getContractStart();
+  Duration getContractLength();
+  ZoneId getTimeZone();
+  String getBillingAddress();
+}
